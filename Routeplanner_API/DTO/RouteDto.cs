aa85022095d1ce29652dcdd49ffcb37376391fcf
mnarijnm <mnarijnm@gmail.com>
2025-05-11 15:51:19 +0200

    Add DTO classes for route management

---
+++ Routeplanner_API/DTO/RouteDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Routeplanner_API.DTO
+{
+    public class RouteDto
+    {
+        public int RouteId { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public int? CreatedBy { get; set; }
+        public bool? IsPrivate { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}

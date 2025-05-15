ffa99d48cc166e8f1a52efa752eb1f810c8c47e8
mnarijnm <mnarijnm@gmail.com>
2025-05-15 15:46:26 +0200

    Update AutoMapper configuration for Location mappings

--- Routeplanner_API/Mappers/LocationProfile.cs
+++ Routeplanner_API/Mappers/LocationProfile.cs
@@ -17,15 +17,15 @@ namespace Routeplanner_API.Mappers
             // Mapping from CreateLocationDto to Location (EF Core entity)
             CreateMap<CreateLocationDto, Location>()
                 .ForMember(dest => dest.LocationId, opt => opt.Ignore()) // Don't map ID for creation
-                .ForMember(dest => dest.UserId, opt => opt.Ignore())    // Assuming UserId is handled separately or not set directly from this DTO
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId)) // Map UserId from DTO so automapper can handle it and set userId to Location
                 .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
                 .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow));
 
             // Mapping from UpdateLocationDto to Location (EF Core entity)
             CreateMap<UpdateLocationDto, Location>()
                 .ForMember(dest => dest.LocationId, opt => opt.Ignore()) // ID is typically from route, not DTO
-                .ForMember(dest => dest.UserId, opt => opt.Ignore())    // Assuming UserId is handled separately
-                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UserId, opt => opt.Ignore()) // Don't map UserId for update
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore()) // Don't change CreatedAt on update
                 .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow)) // Set UpdatedAt on update
                 .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null)); // Only map non-null properties for updates
         }

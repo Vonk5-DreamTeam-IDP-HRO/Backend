91755f3ac279cfdabe816d8e3fff80c21782f1e5
PaulPeeman <99732964+PaulPeeman@users.noreply.github.com>
2025-06-04 16:17:26 +0200

    Fixed UpdateLocation error.

--- Routeplanner_API/Mappers/LocationProfile.cs
+++ Routeplanner_API/Mappers/LocationProfile.cs
@@ -8,34 +8,37 @@ namespace Routeplanner_API.Mappers
     {
         public LocationProfile()
         {
-            // Source -> Target
             // Mapping from Location (EF Core entity) to LocationDto
             CreateMap<Location, LocationDto>()
                 .ForMember(dest => dest.LocationDetail, opt => opt.MapFrom(src => src.LocationDetail));
 
-            // Mapping from CreateLocationDto to Location (EF Core entity)
+            // Mapping from CreateLocationDto to Location (for creation)
             CreateMap<CreateLocationDto, Location>()
-                .ForMember(dest => dest.LocationId, opt => opt.Ignore()) // Don't map ID for creation
-                // UserId is now passed via context.Items in the Map call from LocationUoW
+                .ForMember(dest => dest.LocationId, opt => opt.Ignore()) // Ignore ID during creation
                 .ForMember(dest => dest.UserId, opt => opt.MapFrom((src, dest, destMember, context) => (Guid?)context.Items["UserId"]))
                 .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
                 .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
                 .ForMember(dest => dest.LocationDetail, opt => opt.MapFrom(src => src.LocationDetail)); // Map nested DTO
 
-            // Mapping from UpdateLocationDto to Location (EF Core entity)
+            // Mapping from UpdateLocationDto to Location (for update)
             CreateMap<UpdateLocationDto, Location>()
-                .ForMember(dest => dest.LocationId, opt => opt.Ignore()) // ID is typically from route, not DTO
-                .ForMember(dest => dest.UserId, opt => opt.Ignore()) // Don't map UserId for update
-                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore()) // Don't change CreatedAt on update
-                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow)) // Set UpdatedAt on update
-                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null)); // Only map non-null properties for updates
+                .ForMember(dest => dest.LocationId, opt => opt.Ignore()) // ID from route, not DTO
+                .ForMember(dest => dest.UserId, opt => opt.Ignore()) // Don't map UserId on update
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore()) // Don't update CreatedAt
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow)) // Set UpdatedAt to now
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null)); // Only map non-null values
 
-            // Mapping for LocationDetail
-            CreateMap<CreateLocationDetailDto, LocationDetail>()
-                .ForMember(dest => dest.LocationDetailsId, opt => opt.Ignore()) // Don't map ID for creation, DB generates it
-                .ForMember(dest => dest.LocationId, opt => opt.Ignore());
+            // Mapping from LocationDetailDto to LocationDetail (fix missing mapping)
+            CreateMap<LocationDetailDto, LocationDetail>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null)); // Optional: ignore nulls
 
+            // Mapping from LocationDetail to LocationDetailDto
             CreateMap<LocationDetail, LocationDetailDto>();
+
+            // Mapping from CreateLocationDetailDto to LocationDetail (for creation)
+            CreateMap<CreateLocationDetailDto, LocationDetail>()
+                .ForMember(dest => dest.LocationDetailsId, opt => opt.Ignore()) // Ignore ID on creation
+                .ForMember(dest => dest.LocationId, opt => opt.Ignore()); // Will be set later in the process if needed
         }
     }
 }

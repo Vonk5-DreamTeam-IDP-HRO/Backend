f75b00819fa111ce6414a3fc232315d9fa1d8cd3
PaulPeeman <99732964+PaulPeeman@users.noreply.github.com>
2025-06-11 15:44:30 +0200

    fixed mistakes

--- Routeplanner_API/Database Queries/ILocationDbQueries.cs
+++ Routeplanner_API/Database Queries/ILocationDbQueries.cs
@@ -1,18 +1,18 @@
-using Routeplanner_API.DTO.Location;
-using Routeplanner_API.Models;
+    using Routeplanner_API.DTO.Location;
+    using Routeplanner_API.Models;
 
-namespace Routeplanner_API.Database_Queries
-{
-    public interface ILocationDbQueries
+    namespace Routeplanner_API.Database_Queries
     {
-        Task<Location?> GetByIdAsync(Guid locationId);
-        Task<IEnumerable<Location>> GetAllAsync();
-        Task<Location> CreateAsync(Location location);
-        Task<Location?> UpdateAsync(Location location); // Returns null if not found
-        Task<bool> DeleteAsync(Guid locationId);
-        Task<IEnumerable<string?>> GetUniqueCategoriesAsync();
-        Task<IEnumerable<SelectableLocationDto>> GetSelectableLocationsAsync();
-        Task<IEnumerable<SelectableLocationDto>> GetAllLocationsFromOneCategoryAsync(string categoryName);
+        public interface ILocationDbQueries
+        {
+            Task<Location?> GetByIdAsync(Guid locationId);
+            Task<IEnumerable<Location>> GetAllAsync();
+            Task<Location> CreateAsync(Location location);
+            Task<Location?> UpdateAsync(Location location); // Returns null if not found
+            Task<bool> DeleteAsync(Guid locationId);
+            Task<IEnumerable<string?>> GetUniqueCategoriesAsync();
+            Task<IEnumerable<SelectableLocationDto>> GetSelectableLocationsAsync();
+            Task<IEnumerable<SelectableLocationDto>> GetAllLocationsFromOneCategoryAsync(string categoryName);
 
+        }
     }
-}
